24f8400a45f5f00b10d93c4527be627175820c1f
danieldrn <daniel.nascimento@escolarmanager.com.br>
2018-09-24 17:47:03 -0300

    T008 - Inclui atalho para iniciar projeto
    npm start

--- controllers/product.js
+++ controllers/product.js
@@ -47,14 +47,4 @@ module.exports = function (app) {
                 });
             });
     });
-
-    exports.put = (req, res, next) => {
-
-        const id = req.params.id;
-
-        res.status(200).send({
-            id: id,
-            item: req.body
-        });
-    };
 }
